59c5cf1c5c00ea03cef5c23089e759cb1e07e87b
Aliaksei Hardzeyenkau <alexei.gordeenkov@gmail.com>
2022-08-25 10:10:11 +0300

    Cloud Config

--- src/main/java/org/podpivas/api/rest/TestLoggingController.java
+++ src/main/java/org/podpivas/api/rest/TestLoggingController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.podpivas.model.TestModel;
 import org.podpivas.service.TestService;
 import org.springframework.context.annotation.Scope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +19,16 @@ import org.springframework.web.context.WebApplicationContext;
 public class TestLoggingController {
 
     private final TestService testService;
+    private final Environment environment;
 
     @GetMapping
     public TestModel test() {
         log.debug("test controller");
         return testService.test();
+    }
+
+    @GetMapping("/prop")
+    public String testProp() {
+        return String.join(",", environment.getActiveProfiles());
     }
 }
